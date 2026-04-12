17968dac33840cf79641a85c05f655b876a7a8ad
Jack-Chesser <chessjac000@gmail.com>
2026-04-12 10:43:23 -0500

    data base of all ACADEMIC buildings no pictures yet for MVP

---
+++ buildings.js
@@ -0,0 +1,56 @@
+// ===============================
+// Academic Building Database
+// Easily expandable for MVP
+// ===============================
+
+var buildings = {
+
+    whipple: {
+        name: "Whipple Hall",
+        coords: [39.732144, -90.247616],   
+        img: ""           
+    },
+
+    schewe: {
+        name: "Schewe Library",
+        coords: [39.731218, -90.246322],
+        img: ""
+    },
+
+    kirby: {
+        name: "Kirby Learning Center",
+        coords: [39.730846, -90.246583],
+        img: ""
+    },
+
+    baxter: {
+        name: "Baxter Hall",
+        coords: [39.730657, -90.247166],
+        img: ""
+    }
+
+    crispin: {
+        name: "Crispin Hall"
+        coords: [39.732182, -90.247925],
+        img: ""
+    }
+
+    mcgaw: {
+        name: "McGaw Fine Arts Center"
+        coords: [39.731802, -90.248638],
+        img: ""
+    }
+
+    bruner: {
+        name: "Bruner Fitness and Recreation Center"
+        coords: [39.728635, -90.246789],
+        img: ""
+    }
+
+    rammelkamp: {
+        name: "Rammelkamp Chapel"
+        coords: [39.731782, -90.246285],
+        img: ""
+    }
+
+};
